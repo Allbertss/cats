24d114635aef9e83d281651677dc76201b800d52
Allbertss <42111918+Allbertss@users.noreply.github.com>
2021-04-05 12:09:27 +0300

    connection to Redis

--- index.php
+++ index.php
@@ -8,12 +8,13 @@
 
 	$redis = new Redis();
 	try {
-		$connectedRedis = $redis->connect(HOSTNAME, PORT);
+		$connectedRedis = $redis->connect('lamp-redis', '6379');
 		if (!$connectedRedis)
 			throw new Exception();
 	} catch (Exception $exception) {
 			die($exception->getMessage());
 	}
 
+	//$redis->flushall(); // deletes all counts & cache
 	$redis->close();
 ?>
